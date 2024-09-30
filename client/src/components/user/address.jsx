a34463154e71b5e9b1c481b4bba610420e964a10
deniPamungkas <pamungkasdeni18@gmail.com>
2024-09-30 12:35:27 +0700

    address

--- client/src/components/user/address.jsx
+++ client/src/components/user/address.jsx
@@ -129,6 +129,8 @@ const Address = ({ setCurrentSelectedAddress, selectedId }) => {
     dispatch(fetchAddresses(user?._id));
   }, [dispatch, user]);
 
+  console.log(addressList);
+
   return (
     <Card>
       <div className="mb-5 p-3 grid grid-cols-1 sm:grid-cols-2  gap-2">
